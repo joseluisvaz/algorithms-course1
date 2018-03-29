f49ff17d29f04adf5d2629189eebd060ed18f276
joseluisvaz <joseluisv1201@gmail.com>
2018-03-28 18:55:41 -0600

    First implementation of three partition quick sort

--- week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
 
 using std::vector;
 using std::swap;
@@ -20,19 +19,29 @@ int partition2(vector<int> &array, int p, int r) {
   return i;
 }
 
-int partition3(vector<int> &array, int p, int r) {
+vector<int> partition3(vector<int> &array, int p, int r) {
 
-  int pivot = array[p];
-  int i = p;
+  int pivot = array[r];
+  int i = p - 1;
 
-  for (int j = p + 1; j <= r; j++) {
-    if (array[j] <= pivot) {
+  for (int k = p; k < r; k++) {
+    if (array[k] < pivot) {
       i++;
-      swap(array[j], array[i]);
+      swap(array[i], array[k]);
     }
   }
-  swap(array[p], array[i]);
-  return i;
+
+  int j = i;
+  for (int k = i; k < r; k++) {
+    if (array[k] == pivot) {
+      j++;
+      swap(array[j], array[k]);
+    }
+  }
+
+  swap(array[j + 1], array[r]);
+
+  return vector<int> {i + 1, j + 1};
 }
 
 void randomized_quick_sort(vector<int> &array, int p, int r) {
@@ -41,11 +50,27 @@ void randomized_quick_sort(vector<int> &array, int p, int r) {
   }
 
   int rand_index = p + rand() % (r - p + 1);
+
   swap(array[p], array[rand_index]);
   int q = partition2(array, p, r);
 
   randomized_quick_sort(array, p, q - 1);
   randomized_quick_sort(array, q + 1, r);
+}
+
+void randomized_quick_sort3(vector<int> &array, int p, int r) {
+
+  if (p >= r) {
+    return;
+  }
+
+  int rand_index = p + rand() % (r - p + 1);
+  swap(array[rand_index], array[r]);
+
+  vector<int> q = partition3(array, p, r);
+
+  randomized_quick_sort3(array, p, q[0] - 1);
+  randomized_quick_sort3(array, q[1] + 1, r);
 }
 
 int main() {
@@ -55,7 +80,7 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  randomized_quick_sort(a, 0, a.size() - 1);
+  randomized_quick_sort3(a, 0, a.size() - 1);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
