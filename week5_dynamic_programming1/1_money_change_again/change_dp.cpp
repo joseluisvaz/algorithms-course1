4ccd1c48e83b9b02ec429510272fc6929b585bf3
jose luis <joseluis.vaz.e@gmail.com>
2018-07-18 13:17:36 -0700

    Implementatoin of coins with greedy algorithms

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <vector>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+/* Write a program which gives change to an amount of money with coins 
+of the following denominations:
+1, 3, 4
+*/
+
+using namespace std;
+
+// Global Variables
+vector<int> denominations = {4, 3, 1};
+
+vector<int> get_change(int money) {
+  vector<int> used_coins;
+  for(auto coin: denominations) {
+    while (coin <= money) {
+      used_coins.push_back(coin);
+      money -= coin;
+    }
+  }
+  return used_coins;
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  int money;
+  std::cin >> money;
+
+  vector<int> used_coins = get_change(money);
+  for (auto coin: used_coins) {
+    cout << "used coin: "  << coin << endl;
+  }
 }
