8542c92d58c976c081aa1298376f3ba7e98f5574
jose luis <joseluisv1201@gmail.com>
2018-07-25 18:24:27 -0700

    implementLeast common subsequence

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
+
+
+// Dynamic programming algorithm for the longest subsequence
 
 int lcs2(vector<int> &a, vector<int> &b) {
-  //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+  int n = a.size();
+  int m = b.size();
+  int S[n][m];   // Solution matrix
+
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < m; j++) {
+
+      if (a[i] == b[j]) {
+        if (i == 0 && j == 0){
+          S[i][j] = 1;
+          continue;
+        }
+        if (i == 0) {
+          S[i][j] = S[i][j-1] + 1;
+          continue;
+        }
+        if (j == 0) {
+          S[i][j] = S[i-1][j] + 1;
+          continue;
+        }
+
+        S[i][j] = max(max(S[i-1][j-1], S[i-1][j]), S[i][j-1]) + 1;
+      } 
+      else {
+        if (i == 0 && j == 0){
+          S[i][j] = 0;
+          continue;
+        }
+        if (i == 0) {
+          S[i][j] = S[i][j-1];
+          continue;
+        }
+        if (j == 0) {
+          S[i][j] = S[i-1][j];
+          continue;
+        }
+        S[i][j] = max(max(S[i-1][j-1], S[i-1][j]), S[i][j-1]);
+      }
+    }
+  }
+
+  return S[n - 1][m - 1];
 }
 
 int main() {
